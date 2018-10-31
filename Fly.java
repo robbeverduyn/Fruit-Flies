b67741c53baece5a12c9b7e3cdbd65935e1a9598
BavoAndiessen <bavo.andriessen@gmail.com>
2018-10-31 11:17:55 +0100

    Add files via upload

---
+++ Fly.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fly here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fly extends Actor
+{
+    boolean male = true;
+    public void act() 
+    {
+        
+        move(10);
+        setRotation(Greenfoot.getRandomNumber(360));
+        
+    }   
+    
+    
+    public Fly(boolean male) {
+        
+        if (male) {
+            setImage("maleFly.png");
+        } else {
+            setImage("femaleFly.png");
+        }
+    }
+}
